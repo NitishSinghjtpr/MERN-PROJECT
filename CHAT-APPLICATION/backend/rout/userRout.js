9368c31b39f3e997e9064e051612cccd675f3d81
Nitish Kumar <nitishsinghjtpr@gmail.com>
2026-02-24 12:08:34 +0530

    current chatters find code compelet

--- CHAT-APPLICATION/backend/rout/userRout.js
+++ CHAT-APPLICATION/backend/rout/userRout.js
@@ -1,9 +1,10 @@
 import express from 'express'
 import isLogin from '../middleware/isLogin.js';
-import { getUserBySearch } from './../routControlers/userHandler.js';
+import { getCurrentChatters, getUserBySearch } from './../routControlers/userHandler.js';
 
 const router=express.Router();
 
 router.get('/search',isLogin,getUserBySearch)
+router.get('/currentchatters',isLogin,getCurrentChatters)
 
 export default router;
--- CHAT-APPLICATION/backend/routControlers/userHandler.js
+++ CHAT-APPLICATION/backend/routControlers/userHandler.js
@@ -1,3 +1,4 @@
+import Conversation from "../Models/conversationModel.js";
 import User from "../Models/userModel.js"; 
 
 export const getUserBySearch = async (req, res) => {
@@ -20,6 +21,55 @@ export const getUserBySearch = async (req, res) => {
       .select("email");
 
     res.status(200).send(user);
+  } catch (error) {
+    res.status(500).send({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+export const getCurrentChatters = async (req, res) => {
+  try {
+    const currentUserID = req.user._id;
+
+    const currentChatters = await Conversation.find({
+      participants: currentUserID,
+    }).sort({
+      updatedAt: -1,
+    });
+
+    if (!currentChatters || currentChatters.length === 0)
+      return res.status(200).send([]);
+
+    const participantsIDS = currentChatters.reduce((ids, convo) => {
+      // ensure participants is always an array
+      const participantsArray = Array.isArray(convo.participants)
+        ? convo.participants
+        : [convo.participants];
+
+      const otherParticipents = participantsArray.filter(
+        (id) => id.toString() !== currentUserID.toString()
+      );
+
+      return [...ids, ...otherParticipents];
+    }, []);
+
+    const otherParticipentsIDS = participantsIDS.filter(
+      (id) => id.toString() !== currentUserID.toString()
+    );
+
+    const users = await User.find({
+      _id: { $in: otherParticipentsIDS },
+    })
+      .select("email") 
+      .select("-password");
+
+    const orderedUsers = otherParticipentsIDS.map((id) =>
+      users.find((user) => user._id.toString() === id.toString())
+    );
+
+    res.status(200).send(orderedUsers);
   } catch (error) {
     res.status(500).send({
       success: false,
